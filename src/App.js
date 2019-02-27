f49d2c7f1d13786ee129c8807a51cb8f02c79878
Patrick Heflin <patrickslx@yahoo.com>
2019-02-27 17:46:08 -0600

    imported & initiated nav and homepage components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+
+import HomePage from './Components/HomePage'
+import Nav from './Components/Nav'
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				
-     		 </div>
+				<Nav />
+				<HomePage />
+			</div>
 		);
 	}
 }
