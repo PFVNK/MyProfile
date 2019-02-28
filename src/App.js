bff4686f8988f733eaf4e51917b8fff97dabc8d8
Patrick Heflin <patrickslx@yahoo.com>
2019-02-28 13:10:33 -0600

    set up react router and rendered new components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import HomePage from './Components/HomePage'
 import Nav from './Components/Nav'
+import About from './Components/About'
+import Projects from './Components/Projects'
+import Contact from './Components/Contact'
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Nav />
-				<HomePage />
-			</div>
+			<Router>
+				<div className="App">
+					<Route path='/' component={Nav} />
+					<Route path='/' exact component={HomePage} />
+					<Route path='/about' exact component={About} />
+					<Route path='/projects' exact component={Projects} />
+					<Route path='/contact' exact component={Contact} />
+				</div>
+			</Router>
 		);
 	}
 }
