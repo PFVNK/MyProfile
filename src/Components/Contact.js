c3f0566b1036547a25737dcab4de3862be5a1aea
Patrick Heflin <patrickslx@yahoo.com>
2019-02-28 13:09:17 -0600

    contact component

---
+++ src/Components/Contact.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import '../App.css'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div className='contact-text-container'>
+                <div className='contact-text'>
+                    <h1>CONTACT</h1>
+                    <p>Feel free to contact me anytime!</p>
+                    <a href="mailto: patrickslx@yahoo.com">patrickslx@yahoo.com</a>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+export default Contact
