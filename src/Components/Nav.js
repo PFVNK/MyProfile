903f271c1902aa869e07e38cc6c874e2cc175cdf
Patrick Heflin <patrickslx@yahoo.com>
2019-02-28 13:08:41 -0600

    added react router links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 class Nav extends Component {
     render() {
         return (
             <div className='nav-nav'>
-                <header className='nav-title'>
-                    <span className='nav-badge'>
-                        <p>PH</p>
-                    </span>
-                    <h1>Patrick Heflin</h1>
-                </header>
+                <NavLink to={'/'}>
+                    <header className='nav-title'>
+                        <span className='nav-badge'>
+                            <p>PH</p>
+                        </span>
+                        <h1>Patrick Heflin</h1>
+                    </header>
+                </NavLink>
+
 
                 <nav className='nav-items'>
                     <ul>
-                        <li>ABOUT</li>
-                        <li>PROJECTS</li>
-                        <li>CONTACT</li>
+                        <li><NavLink to={'/about'}>ABOUT</NavLink></li>
+                        <li><NavLink to={'/projects'}>PROJECTS</NavLink></li>
+                        <li><NavLink to={'/contact'}>CONTACT</NavLink></li>
                     </ul>
                 </nav>
             </div>
