67e3c76d87edd7f86fdbd863888b566a99f7c898
Patrick Heflin <patrickslx@yahoo.com>
2019-03-02 14:53:14 -0600

    footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+class Footer extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        const activeClass = this.props.location.pathname === '/' ? 'footer-icons' : 'sidebar-icons'
+        console.log(activeClass)
+        return (
+            <div className={activeClass}>
+                <a href='https://github.com/PFVNK'><FontAwesomeIcon icon={faGithub} className='footer-github-icon' /></a>
+                <a href='https://twitter.com/PFVNK'><FontAwesomeIcon icon={faTwitter} className='footer-twitter-icon' /></a>
+                <a href='https://www.facebook.com/patrick.heflin.1'><FontAwesomeIcon icon={faFacebook} className='footer-facebook-icon' /></a>
+            </div>
+        )
+    }
+}
+
+
+export default Footer
