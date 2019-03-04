0aec2012651afc260c68ab254a045d0f58ba389f
Patrick Heflin <patrickslx@yahoo.com>
2019-03-04 15:46:19 -0600

    updated about information

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,9 +9,9 @@ class About extends Component {
                 <div className='about-text'>
                     <h1>ABOUT</h1>
                     <p>Hi, I'm <span className='about-text-highlight'>Patrick Heflin</span></p>
-                    <p>I am a Web Developer from <span className='about-text-highlight'>Houston, Texas</span>. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo obcaecati officiis hic ex eveniet officia autem temporibus atque dolorem deleniti.</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur pariatur minima, perferendis nobis doloribus voluptates a voluptatem assumenda nisi reprehenderit dolore! Nam rerum eligendi ex labore excepturi perspiciatis dicta fugiat earum id ratione cum, eius placeat quas nemo vitae eaque.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ab culpa illum optio nostrum facere iure id alias ipsa corrupti sint, voluptatem est expedita nam amet autem nobis. Fugit deleniti similique rem nisi tenetur ducimus dolor natus temporibus! Eos, perspiciatis facere harum numquam aut cumque. Ex architecto earum similique ea voluptas, minus quod quos. Ducimus eligendi inventore odio sed aliquam.</p>
+                    <p>I am a Web Developer from <span className='about-text-highlight'>Houston, Texas</span>. I pride myself on making fast, reliable, intuitive and responsive applications.</p>
+                    <p>I specialize in building applications specific to the business needs of my clients. I have a huge passion for helping others and solving problems.</p>
+                    <p> I enjoy work with mobile app creation, front-end/back-end web, database/server management, and graphic design.</p>
                 </div>
             </div>
         )
