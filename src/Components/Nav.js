339c1c77122fc2694fc6bff87df14aee44b14bfd
Patrick Heflin <patrickslx@yahoo.com>
2019-03-04 15:58:57 -0600

    added dummy element

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,25 +4,28 @@ import { NavLink } from 'react-router-dom'
 class Nav extends Component {
     render() {
         return (
-            <div className='nav-nav'>
-                <NavLink to={'/'}>
-                    <header className='nav-title'>
-                        <span className='nav-badge'>
-                            <p>PH</p>
-                        </span>
-                        <h1>Patrick Heflin</h1>
-                    </header>
-                </NavLink>
+            <React.Fragment>
+                <div className='nav-nav'>
+                    <NavLink to={'/'}>
+                        <header className='nav-title'>
+                            <span className='nav-badge'>
+                                <p>PH</p>
+                            </span>
+                            <h1>Patrick Heflin</h1>
+                        </header>
+                    </NavLink>
 
 
-                <nav className='nav-items'>
-                    <ul>
-                        <li><NavLink to={'/about'}>ABOUT</NavLink></li>
-                        <li><NavLink to={'/projects'}>PROJECTS</NavLink></li>
-                        <li><NavLink to={'/contact'}>CONTACT</NavLink></li>
-                    </ul>
-                </nav>
-            </div>
+                    <nav className='nav-items'>
+                        <ul>
+                            <li><NavLink to={'/about'}>ABOUT</NavLink></li>
+                            <li><NavLink to={'/projects'}>PROJECTS</NavLink></li>
+                            <li><NavLink to={'/contact'}>CONTACT</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className='nav-dummy'></div>
+            </React.Fragment>
         )
     }
 }
