6b00aac8856ed656fa1d240636b51f089012d620
Patrick Heflin <patrickslx@yahoo.com>
2019-03-05 09:06:44 -0600

    add background image for mobile

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import '../App.css'
 
 import shortglitch from '../shortglitch1.mp4'
+import brick from '../brick.jpg'
 
 class HomePage extends Component {
     constructor(props) {
@@ -21,6 +22,10 @@ class HomePage extends Component {
                         <h1>I make websites</h1>
                     </div>
                 </main>
+
+                <div className='home-background-image'>
+                    <img src={brick} alt="image of patrick heflin" />
+                </div>
 
                 <video className="home-background-video" loop autoPlay muted>
                     <source src={this.state.videoURL} type="video/mp4" />
