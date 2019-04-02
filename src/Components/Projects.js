d9fb284cd3d34d795063b487d03ce4de4f231730
Patrick Heflin <patrickslx@yahoo.com>
2019-04-02 06:50:07 -0500

    updated project url

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,7 @@ class Projects extends Component {
                     <h1>PROJECTS</h1>
                     <a href='https://www.thesynth.live'><h2>TheSynth</h2></a>
                     <p>Synth buitlt with React and Tone.js, styled with Sass. Choose from multiple voices and oscillators.</p>
-                    <a href="https://pizza-dough.com"><h2>Pizza Dough</h2></a>
+                    <a href="https://www.pizza-dough.com/"><h2>Pizza Dough</h2></a>
                     <p>Tip tracker for delivery drivers. Positions logged in drivers on leader board based on amount of tips made. React front-end and Node/MongoDB backend to track driver data.</p>
                 </div>
 
