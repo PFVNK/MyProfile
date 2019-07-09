bab6a90b35e348a1b6fce7477b09f5b539ae6072
Patrick Heflin <patrickslx@yahoo.com>
2019-07-09 12:38:58 -0500

    Add new project (get-stuff)

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,9 +9,11 @@ class Projects extends Component {
                 <div className='projects-text'>
                     <h1>PROJECTS</h1>
                     <a href='https://www.thesynth.live'><h2>TheSynth</h2></a>
-                    <p>Synth buitlt with React and Tone.js, styled with Sass. Choose from multiple voices and oscillators.</p>
+                    <p>Synth built with React and Tone.js, styled with Sass. Choose from multiple voices and oscillators.</p>
                     <a href="https://www.pizza-dough.com/"><h2>Pizza Dough</h2></a>
                     <p>Tip tracker for delivery drivers. Positions delivery drivers on leader board based on amount of tips made. React front-end and Node/MongoDB backend to track driver data.</p>
+                    <a href="https://www.get-stuff.net"><h2>Get Stuff</h2></a>
+                    <p>Follow your favorite Craiglist searches and get notified when new items are added. Built with a React front-end and Node/Express backend</p>
                 </div>
 
             </div>
