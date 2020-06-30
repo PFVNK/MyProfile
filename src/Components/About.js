dba3537ae42eba831e1b4d813275f4062f512733
Patrick Heflin <patrickslx@yahoo.com>
2020-06-30 15:21:57 -0500

    add homepage image

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
                     <p>Hi, I'm <span className='about-text-highlight'>Patrick Heflin</span></p>
                     <p>I am a Web Developer from <span className='about-text-highlight'>Houston, Texas</span>. I pride myself on making fast, reliable, intuitive and responsive applications.</p>
                     <p>I specialize in building applications specific to the business needs of my clients. I have a huge passion for helping others and solving problems.</p>
-                    <p> I enjoy work with mobile app creation, front-end/back-end web, database/server management, and graphic design.</p>
+                    <p> I enjoy work with desktop/mobile app creation, front-end/back-end web, database/server management, and graphic design.</p>
                 </div>
             </div>
         )
