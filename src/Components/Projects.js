d7502e75b7e8cd1a6f5580b1e37e9f37b759b7ac
Patrick Heflin <patrickslx@yahoo.com>
2020-07-01 10:56:00 -0500

    2020

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,6 +14,12 @@ class Projects extends Component {
                     <p>Tip tracker for delivery drivers. Positions delivery drivers on leader board based on amount of tips made. React front-end and Node/MongoDB backend to track driver data.</p>
                     <a href="https://www.get-stuff.net"><h2>Get Stuff</h2></a>
                     <p>Follow your favorite Craiglist searches and get notified when new items are added. Built with a React front-end and Node/Express backend</p>
+                    <a href="http://www.mlbd3.com/"><h2>MLBD3</h2></a>
+                    <p>View offensive statistics from any MLB team in an interactive bubble chart. Statisics include batting average, total hits and home runs. Statistical data is scraped and processed with Node/Express and Cheerio. Built with React and D3, styled with Sass.</p>
+                    <a href="http://www.willyburger.net/"><h2>Willy Burger</h2></a>
+                    <p>Website for local burger restaurant built with React and React-Router.</p>
+                    <a href="https://first-choice-pressure-washing.herokuapp.com/"><h2>First Choice Pressure Washing</h2></a>
+                    <p>Company website for pressure washing company built with React and React-router. Styled with Sass</p>
                 </div>
 
             </div>
